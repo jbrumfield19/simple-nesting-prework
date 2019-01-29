ba2e4a641f9f23ba048a30bafa5a48fc55c7e60c
Jessica Brumfield <jbrumfield1999@gmail.com>
2019-01-29 20:31:23 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -127,5 +127,5 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-adding_to_dennis = programmer_hash[:dennis_ritchie][:languages].push('assembly')
+adding_to_dennis = programmer_hash.update(:alan_kay =>['C', 'Assembly'])
 end
